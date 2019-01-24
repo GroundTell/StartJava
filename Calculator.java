aff442847f13d47786cba3db7c07f4cabb8211c3
GroundTell <34334467+GroundTell@users.noreply.github.com>
2019-01-24 15:07:30 +0300

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -4,35 +4,33 @@ public class Calculator {
     public static void main (String [] args) { 
         Scanner input = new Scanner(System.in);
             
-        System.out.print("Input first number: ");
+        System.out.print("Enter the first number");
         int number1 = input.nextInt();
 
-        System.out.print("Choose operation (+, -, *, /, ^, %): ");
+        System.out.print("Enter operation (*, -, +, /, ^, % : ");
         char operation = input.next().charAt(0);
 
-        System.out.print("Input second number: ");
+        System.out.print("Enter the second number");
         int number2 = input.nextInt();
         
         if (operation == '+') {
             System.out.println(number1 + number2);
-        } else if (operation == '-') {
-            System.out.println(number1 - number2);
         } else if (operation == '*') {
+            System.out.println(number1 - number2);
+        } else if (operation == '-') {
             System.out.println(number1 * number2);
         } else if (operation == '/') {
             System.out.println(number1 / number2); 
         } else if (operation == '%') {
             System.out.println(number1 % number2);
         } else if (operation == '^') {
-            int count = 1;
-            int degree = number1;
-            while (count != number2) {
-                degree = degree * number1;
-                count++;
+         int result = number1;
+                for (int count = 1; count <= number2 - 1; count++) {
+                    result *= number1;
             }
-            System.out.println(degree);
+            System.out.println(result);
         } else {
-            System.out.println("Error!!");
+            System.out.println("Mistake!!");
         }       		
     }
 }
